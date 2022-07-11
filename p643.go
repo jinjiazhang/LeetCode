899af9c545f1bdbbce5b12418327d07be43a298b
jinjiazhang <jinjiazh@qq.com>
2022-07-11 17:22:44 +0800

    easy-p643

---
+++ p643.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func findMaxAverage(nums []int, k int) float64 {
+	sum := 0
+	for i := 0; i < k; i++ {
+		sum += nums[i]
+	}
+
+	maxSum := sum
+	for j := 1; j <= len(nums)-k; j++ {
+		sum = sum - nums[j-1] + nums[j+k-1]
+		if sum > maxSum {
+			maxSum = sum
+		}
+	}
+
+	return float64(maxSum) / float64(k)
+}
---
+++ p643_test.go
@@ -0,0 +1,10 @@
+package leetcode
+
+import "testing"
+
+func Test_643(t *testing.T) {
+	nums := []int{0, 1, 1, 3, 3}
+	k := 4
+	result := findMaxAverage(nums, k)
+	t.Log(result)
+}
